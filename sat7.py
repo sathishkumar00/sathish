d224bf62f7de63140414264c21b92e515492f0fb
sathishkumar00 <47203484+sathishkumar00@users.noreply.github.com>
2019-06-13 19:40:57 +0530

    Create sat7.py

---
+++ sat7.py
@@ -0,0 +1,5 @@
+sk=input()
+l=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for i in range(len(l)):
+    if l[i]==sk:
+        print(i)
