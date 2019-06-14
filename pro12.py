44a8a4dba37f1f5e1200ae82831b31d55f40a6d0
sathishkumar00 <47203484+sathishkumar00@users.noreply.github.com>
2019-06-14 12:19:50 +0530

    Create pro12.py

---
+++ pro12.py
@@ -0,0 +1,10 @@
+nat,qat=map(int,input().split())
+tat=list(map(int,input().split()))
+st=[]
+for i in range(qat):
+    st.append(list(map(int,input().split())))
+for i in st:
+  z=0
+  for j in range(i[0]-1,i[1]):
+      z=z+t[j]
+  print(z)    
