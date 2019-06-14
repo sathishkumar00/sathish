350b67db70babce548da3efb3efaa749dfb3144a
sathishkumar00 <47203484+sathishkumar00@users.noreply.github.com>
2019-06-14 11:01:46 +0530

    Create sat16.py

---
+++ sat16.py
@@ -0,0 +1,14 @@
+Nk=int(input())
+ak=list(map(int,input().split()))
+c=[]
+count=0
+for i1 in ak:
+  if i1 not in c:
+    c1.append(i1)
+for j1 in c:
+  for k1 in a1:
+    if(j1==k1):
+      count=count+1
+  if(count==1):
+    print(j1)
+  count=0
