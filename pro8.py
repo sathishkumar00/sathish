6b5a3c3908f6409f9a6a529cc3dcaea32a3ba93b
sathishkumar00 <47203484+sathishkumar00@users.noreply.github.com>
2019-06-14 11:56:15 +0530

    Update pro8.py

--- pro8.py
+++ pro8.py
@@ -1,9 +1,3 @@
-Sat2=int(input())
-lat1=list(map(int,input().split()))
-count=0
-for i in range(len(lat1)-2):
-    for j in range(i+1,len(lat1)-1):
-        for k in range(j+1,len(l1)):
-            if lat1[i]<l1[j]<l1[k] and i<j<k:
-                count=count+1
-print(count)
+num3=int(input())
+while(num3>=1000)
+print(num3)
